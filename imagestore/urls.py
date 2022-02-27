310efc675bd3820797e5ed3c960b281588940746
trevin-livele <liveletrevin6@gmail.com>
2022-02-28 00:05:54 +0300

    filtered out by category

--- imagestore/urls.py
+++ imagestore/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.imagestore, name='imagestore'),
+    path('<slug:category_slug>/',views.imagestore, name='imagestores_by_category'),
+
 
 
 ]
--- imagestore/views.py
+++ imagestore/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-from .models import Imagestore
+from unicodedata import category
+from webbrowser import get
+from django.forms import SlugField
+from django.shortcuts import get_object_or_404, render
+from .models import Imagestore,Category
 # Create your views here.
 
 
 
-def imagestore(request):
-    image_store = Imagestore.objects.all()
+def imagestore(request,category_slug=None):
+    categories = None
+    image_store = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category,slug=category_slug)
+        image_store = Imagestore.objects.filter(category=categories)
+
+    else:
+        image_store = Imagestore.objects.all()
 
 
     context = {

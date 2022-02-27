422d4c40815019fd442f87ed642d5a082e07ed8e
trevin-livele <liveletrevin6@gmail.com>
2022-02-27 23:33:50 +0300

    added my images section

--- imagestore/views.py
+++ imagestore/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Imagestore
 # Create your views here.
+
+
+
+def imagestore(request):
+    image_store = Imagestore.objects.all()
+
+
+    context = {
+            'image_store' : image_store,
+        }
+    return render(request, 'imagestore/imagestore.html', context)
--- photogalerry/urls.py
+++ photogalerry/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,8 +24,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('imagestore/',include('imagestore.urls')),
     path('about/',views.about, name='about'),
-    path('contact/',views.contact, name='contact'),
 
 
 ] +static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)

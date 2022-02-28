b73116c7f7e7bea107c5995d5c674efce5a79b50
trevin-livele <liveletrevin6@gmail.com>
2022-02-28 19:24:48 +0300

    added static

--- photogalerry/settings.py
+++ photogalerry/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'photogalerry/static',
 ]
